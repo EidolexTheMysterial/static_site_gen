15aed1c2a0af82d2c2ccaae299efa06209f82930
EidolexTheMysterial <tjdow@comcast.net>
2025-04-25 23:25:33 -0700

    Updating build.sh log-text

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-echo "\n[[ main.sh : executing src/main.py with arg "/static_site_gen/" ]]\n\n"
+echo "\n[[ build.sh : executing src/main.py with arg "/static_site_gen/" for GH Pages deployment ]]\n\n"
 python3 src/main.py "/static_site_gen/"
