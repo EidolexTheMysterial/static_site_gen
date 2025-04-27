e1a09fea67ffd2801a862389523f3e378bb4686f
EidolexTheMysterial <tjdow@comcast.net>
2025-04-26 20:52:29 -0700

    Improving main.sh with error-detection, var for public_dir

--- main.sh
+++ main.sh
@@ -1,7 +1,19 @@
 
+public_dir="docs/"
 
+# build static-site for localhost
 echo "\n[[ $0 : executing src/main.py ]]\n\n"
 python3 src/main.py
 
-echo "\n\n[[ $0: starting web server in docs/ ]]\n\n"
-cd docs && python3 -m http.server 8888
+# if no error, start python web server
+if [ $? -eq 0 ]; then
+
+  echo "\n\n[[ $0: starting web server in $public_dir ]]\n\n"
+  cd $public_dir && python3 -m http.server 8888
+
+else
+
+  echo
+  echo "[[ $0 : Error detected, exiting ]]"
+
+fi
