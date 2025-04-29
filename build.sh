a763c231983bea30e1e00ad258b22f77783e1a6c
EidolexTheMysterial <tjdow@comcast.net>
2025-04-28 17:53:30 -0700

    Adding var for repo_name to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-echo "\n[[ $0 : executing src/main.py with arg "/static_site_gen/" for GH Pages deployment ]]\n\n"
-python3 src/main.py "/static_site_gen/"
+
+repo_name="/static_site_gen/"
+
+echo "\n[[ $0 : executing src/main.py with arg '$repo_name' for GH Pages deployment ]]\n\n"
+
+python3 src/main.py $repo_name
