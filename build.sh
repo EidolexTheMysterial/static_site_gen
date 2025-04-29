be4737ca15d065286f76dc0e9a38187cdbd94e54
EidolexTheMysterial <tjdow@comcast.net>
2025-04-28 19:01:40 -0700

    Adjusting sh scripts to show only the script name in logs via cur_script var

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 
+cur_script=${0/#.\//}
+
 repo_name="/static_site_gen/"
 
-echo "\n[[ $0 : executing src/main.py with arg '$repo_name' for GH Pages deployment ]]\n\n"
+echo "\n[[ $cur_script : executing src/main.py with arg '$repo_name' for GH Pages deployment ]]\n\n"
 
 python3 src/main.py $repo_name
--- main.sh
+++ main.sh
@@ -1,19 +1,21 @@
 
+cur_script=${0/#.\//}
+
 public_dir="docs/"
 
 # build static-site for localhost
-echo "\n[[ $0 : executing src/main.py ]]\n\n"
+echo "\n[[ $cur_script : executing src/main.py ]]\n\n"
 python3 src/main.py
 
 # if no error, start python web server
 if [ $? -eq 0 ]; then
 
-  echo "\n\n[[ $0: starting web server in $public_dir ]]\n\n"
+  echo "\n\n[[ $cur_script : starting web server in $public_dir ]]\n\n"
   cd $public_dir && python3 -m http.server 8888
 
 else
 
   echo
-  echo "[[ $0 : Error detected, exiting ]]"
+  echo "[[ $cur_script : Error detected, exiting ]]"
 
 fi
